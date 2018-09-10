42fe387bd5ce0a95d70ae145e37204e15571730e
dude <magnetoex@gmail.com>
2018-09-10 21:58:30 +1000

    basic terraform script now operational against binary lane

---
+++ README.md
@@ -0,0 +1,15 @@
+terraform will define virtual private servers on binary lane
+vagrant will create the virtual private servers
+ansible will configure them
+
+see here for manual interaction with openstack api via nova (handy for troubleshooting):
+https://www.binarylane.com.au/support/solutions/articles/1000026198-api-openstack-command-line-client
+
+manual pre-reqs:
+1. vpc is defined manually in binary lane console; yet to determine api call to do so
+2. ...
+
+deployment steps:
+1. set up vps's
+cd terraform && ../bin/terraform apply
+
---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+# binarylane api key or login password (key preferred)
+variable "openstack_password" {}
+
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name   = "magnetoex@gmail.com"
+  tenant_name = "binarylane"
+  password    = "${var.openstack_password}"
+  auth_url    = "https://api.binarylane.com.au/v2.0"
+  #region      = "RegionOne"
+}
+
+resource "openstack_compute_instance_v2" "host_mel_1" {
+  name            = "host_mel_1"
+  image_id        = "9310ba82-31c5-4cd3-add3-d7dfc6fc86b2"
+  flavor_id       = "plan-768"
+  #key_pair        = "my_key_pair_name"
+  #security_groups = ["default"]
+
+#  network {
+#    name = "vpc_melb"
+#  }
+}
+
+resource "openstack_compute_instance_v2" "host_mel_2" {
+  name            = "host_mel_2"
+  image_id        = "9310ba82-31c5-4cd3-add3-d7dfc6fc86b2"
+  flavor_id       = "plan-768"
+  #key_pair        = "my_key_pair_name"
+  #security_groups = ["default"]
+
+ # network {
+ #   name = "vpc_mel"
+ # }
+}
